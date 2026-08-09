9232411ca512e49c0d1ade67b9c72045f4b7c314
7.studio <mail@julianejeske.com>
2026-08-09 11:53:18 +0200

    add plain form inputs

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,45 @@ function App() {
   return (
     <>
       <h1>Theme Creator</h1>
-      // display colors here
         <main>
           <h1>Color Cards</h1>
+         <form>
+        <label for="role">Role</label>
+        <input
+          type="text"
+          id="role"
+          name="role"
+          placeholder="primary"
+        />
+
+        <label for="hex">Hex value</label>
+        <input
+          type="color"
+          id="hex-color"
+          name="hex-color"
+        />
+        <input
+          type="text"
+          id="hex"
+          name="hex"
+          placeholder="#ff0000"
+        />
+
+       <label for="contrast-text">Contrast text</label>
+        <input
+          type="color"
+          id="contrast-color"
+          name="contrast-color"
+        />
+        <input
+          type="text"
+          id="contrast-text"
+          name="contrast-text"
+          placeholder="#ffffff"
+        />
+
+        <button type="submit">Add color</button>
+      </form>
            {initialColors.map((color) => (
               <Color
               key={color.hex}
